21bd1f12099d97d2cfbefeb4f21cd51a3e37e06e
Coco-Lapin <vdp.corentin@gmail.com>
2026-05-07 10:36:51 +0200

    🎨Ajout d'un client pour test la page de first connection + ligne pour drop la table

--- code/database/connection.sql
+++ code/database/connection.sql
@@ -12,9 +12,11 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE INDEX IF NOT EXISTS idx_users_username ON users (username);
 
 -- ajout commentaire test webhook
-INSERT INTO users (username, password, is_admin) VALUES
+INSERT INTO users (username, password, is_admin, is_firstConnexion) VALUES
 ('vdp_corentin', '$argon2id$v=19$m=65536,t=3,p=1$REPLACE_WITH_REAL_HASH$REPLACE_WITH_REAL_HASH', TRUE, FALSE),
 ('cnv_evan', '$argon2id$v=19$m=65536,t=3,p=1$REPLACE_WITH_REAL_HASH$REPLACE_WITH_REAL_HASH', TRUE, FALSE),
-('client_test', '$argon2id$v=19$m=65536,t=3,p=1$REPLACE_WITH_REAL_HASH$REPLACE_WITH_REAL_HASH', FALSE, FALSE)
+('client_test', '$argon2id$v=19$m=65536,t=3,p=1$REPLACE_WITH_REAL_HASH$REPLACE_WITH_REAL_HASH', FALSE, FALSE),
+('client_test_2', '$argon2id$v=19$m=65536,t=3,p=1$REPLACE_WITH_REAL_HASH$REPLACE_WITH_REAL_HASH', FALSE, TRUE)
 ON CONFLICT (username) DO NOTHING;
 
+DROP TABLE IF EXISTS users; --drop la table
