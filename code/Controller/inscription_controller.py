ffae3645fd1d5d47db1892c9a9f6e1d02c0e624c
Florian <98989004+xen0r-star@users.noreply.github.com>
2026-09-24 11:20:19 +0200

    :file File restructuring and removal of unnecessary code

--- code/Controller/inscription_controller.py
+++
@@ -1,6 +0,0 @@
-import customtkinter as ctk
-
-def open_inscription():
-    win = ctk.CTkToplevel()
-    win.title("Inscription")
-
--- code/Controller/menu_controller.py
+++
@@ -1,8 +0,0 @@
-import customtkinter as tk
-
-
-def afficher_menu():
-    frame_connexion.pack_forget()  # cache
-    frame_menu.pack()              # affiche
-
-tk.Button(root, text="Connexion", command=afficher_menu)
